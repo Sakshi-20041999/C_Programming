07168e3b7ab3745bf7aba638ec4041588a4d63c8
Sakshi Jadhav <jadhavsakshi3495@gmail.com>
2026-01-20 14:42:54 +0530

    Add files via upload

---
+++ Assignments/Assighnment_54/Program_54_3.c
@@ -0,0 +1,42 @@
+///////////////////////////////////////////////////////////
+// Required Header Files :
+///////////////////////////////////////////////////////////
+#include<stdio.h>
+
+///////////////////////////////////////////////////////////
+// Function Name : SumOdd
+// Description   : Calculates sum of odd elements
+// Input         : Integer array, Size
+// Author        : Sakshi Jadhav
+// Date          : 06/01/2026
+///////////////////////////////////////////////////////////
+int SumOdd(void *arr, int iSize)
+{
+    int i, sum = 0;
+
+    for(i = 0; i < iSize; i++)
+    {
+        if(*((int*)arr + i) % 2 != 0)
+            sum += *((int*)arr + i);
+    }
+    return sum;
+}
+
+///////////////////////////////////////////////////////////
+// Entry Point Function
+///////////////////////////////////////////////////////////
+int main()
+{
+    int arr[] = {10,15,20,25,30};
+
+    int ret = SumOdd(arr,5);
+    printf("%d", ret);
+
+    return 0;
+}
+
+/*
+Test Case :
+Input  : 10 15 20 25 30
+Output : 40
+*/
---
+++ Assignments/Assighnment_54/Program_54_5.c
@@ -0,0 +1,43 @@
+///////////////////////////////////////////////////////////
+// Required Header Files :
+///////////////////////////////////////////////////////////
+#include<stdio.h>
+
+///////////////////////////////////////////////////////////
+// Function Name : Min
+// Description   : Finds minimum element in array
+// Input         : Integer array, Size
+// Author        : Sakshi Jadhav
+// Date          : 06/01/2026
+///////////////////////////////////////////////////////////
+int Min(void *arr, int iSize)
+{
+    int i;
+    int min = *((int*)arr);
+
+    for(i = 1; i < iSize; i++)
+    {
+        if(*((int*)arr + i) < min)
+            min = *((int*)arr + i);
+    }
+    return min;
+}
+
+///////////////////////////////////////////////////////////
+// Entry Point Function
+///////////////////////////////////////////////////////////
+int main()
+{
+    int arr[] = {10,20,5,40,50};
+
+    int ret = Min(arr,5);
+    printf("%d", ret);
+
+    return 0;
+}
+
+/*
+Test Case :
+Input  : 10 20 5 40 50
+Output : 5
+*/
